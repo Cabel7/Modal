a37c17a7b0c30c18c7162259ac1321cd91479d0d
Askerin <134921117+Cabel7@users.noreply.github.com>
2024-03-06 17:54:13 +0530

    Update run3.py

--- run3.py
+++ run3.py
@@ -17,12 +17,12 @@ volume = modal.NetworkFileSystem.new().persisted("stable-diffusion-webui")
         pip install -q torch==2.0.1+cu118 torchvision==0.15.2+cu118 torchaudio==2.0.2+cu118 torchtext==0.15.2 torchdata==0.6.1 --extra-index-url https://download.pytorch.org/whl/cu118 && \
         pip install -q xformers==0.0.20 triton==2.0.0 packaging==23.1"
     ),
-    network_file_systems={"/content/ui": volume},
-    gpu=modal.gpu.T4(count=2), 
+    network_file_systems={"/content/stable-diffusion-webui": volume},
+    gpu="T4",
     timeout=60000,
 )
 async def run():
-    os.system(f"git clone -b v2.4 https://github.com/camenduru/stable-diffusion-webui /content/stable-diffusion-webui")
+    os.system(f"git clone -b v2.6 https://github.com/camenduru/stable-diffusion-webui /content/stable-diffusion-webui")
     os.system(f"git clone https://github.com/camenduru/sd-civitai-browser /content/stable-diffusion-webui/extensions/sd-civitai-browser")
     os.system(f"git clone https://github.com/DominikDoom/a1111-sd-webui-tagcomplete.git /content/stable-diffusion-webui/extensions/tag-autocomplete") 
     os.system(f"git clone https://github.com/camenduru/stable-diffusion-webui-huggingface /content/stable-diffusion-webui/extensions/stable-diffusion-webui-huggingface") 
