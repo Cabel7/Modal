806ce7de9567545a3a5730ab6f979d611418c0a4
Askerin <134921117+Cabel7@users.noreply.github.com>
2024-04-06 22:32:53 +0530

    Update run3.py

--- run3.py
+++ run3.py
@@ -1,7 +1,7 @@
 import modal, os, sys, shlex
 
 stub = modal.Stub("stable-diffusion-webui")
-volume = modal.NetworkFileSystem.new().persisted("stable-diffusion-webui")
+volume = modal.NetworkFileSystem.from_name("stable-diffusion-webui")
 
 @stub.function(
     image=modal.Image.from_registry("nvidia/cuda:12.2.0-base-ubuntu22.04", add_python="3.11")
