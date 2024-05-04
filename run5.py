2b520c4a3e4bf8caab34c79cf95817bd0befb40e
Askerin <134921117+Cabel7@users.noreply.github.com>
2024-05-05 02:38:26 +0530

    Update run5.py

--- run5.py
+++ run5.py
@@ -1,7 +1,7 @@
 import modal, os, sys, shlex, subprocess
 
 app = modal.App("stable-diffusion-webui")
-volume = modal.NetworkFileSystem.from_name("stable-diffusion-webui", create_if_missing=True)
+#volume = modal.NetworkFileSystem.from_name("stable-diffusion-webui", create_if_missing=True)
 
 @app.function(
     image=(
@@ -25,12 +25,13 @@ volume = modal.NetworkFileSystem.from_name("stable-diffusion-webui", create_if_m
         )
         .run_commands("pip install -q torch==2.0.1+cu118 torchvision==0.15.2+cu118 torchaudio==2.0.2+cu118 torchtext==0.15.2 torchdata==0.6.1 --extra-index-url https://download.pytorch.org/whl/cu118")
         .run_commands("pip install -q xformers==0.0.20 triton==2.0.0 packaging==23.1")
-    ),
-    network_file_systems={"/content/stable-diffusion-webui": volume},
+        .run_commands("git clone -b v2.6 https://github.com/camenduru/stable-diffusion-webui /content/stable-diffusion-webui")
+    )
+)
+@app.function(  
     gpu="T4",
     timeout=60000,
 )
-            
 def run():
     # os.system(f"git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git /content/stable-diffusion-webui")
     # os.system(f"git clone https://github.com/Cabel7/Webui /content/stable-diffusion-webui")
