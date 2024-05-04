dcf35f0f54d85dc5e978c2d522ac936355721cc6
Askerin <134921117+Cabel7@users.noreply.github.com>
2024-05-05 02:51:57 +0530

    Update run5.py

--- run5.py
+++ run5.py
@@ -1,14 +1,13 @@
 import modal, os, sys, shlex, subprocess
 
-app = modal.App("stable-diffusion-webui")
+#app = modal.App("stable-diffusion-webui")
 #volume = modal.NetworkFileSystem.from_name("stable-diffusion-webui", create_if_missing=True)
 
-@app.function(
-    image=(
-        modal.Image.from_registry(
-            "nvidia/cuda:12.2.0-base-ubuntu22.04", add_python="3.11"
-        )
-        .apt_install(
+image=(
+    modal.Image.from_registry(
+        "nvidia/cuda:12.2.0-base-ubuntu22.04", add_python="3.11"
+    )
+    .apt_install(
             "software-properties-common",
             "git",
             "git-lfs",
@@ -28,22 +27,28 @@ app = modal.App("stable-diffusion-webui")
         .run_commands("git clone -b v2.6 https://github.com/camenduru/stable-diffusion-webui /content/stable-diffusion-webui")
     )
 )
-@app.function(  
-    gpu="T4",
-    timeout=60000,
-)
+
+app = modal.App("stable-diffusion-webui", image=image)
+
+PORT = 8000
+MINUTES = 60
+
+@app.function(gpu="T4", timeout=60000)
+@modal.web_server(port=PORT, startup_timeout=3 * MINUTES)
+
+
 def run():
     # os.system(f"git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git /content/stable-diffusion-webui")
     # os.system(f"git clone https://github.com/Cabel7/Webui /content/stable-diffusion-webui")
     
-    os.system(f"git clone -b v2.6 https://github.com/camenduru/stable-diffusion-webui /content/stable-diffusion-webui")
+    # os.system(f"git clone -b v2.6 https://github.com/camenduru/stable-diffusion-webui /content/stable-diffusion-webui")
     os.system(f"git clone https://github.com/camenduru/sd-civitai-browser /content/stable-diffusion-webui/extensions/sd-civitai-browser")
     os.system(f"git clone https://github.com/DominikDoom/a1111-sd-webui-tagcomplete.git /content/stable-diffusion-webui/extensions/tag-autocomplete") 
     os.system(f"git clone https://github.com/camenduru/stable-diffusion-webui-huggingface /content/stable-diffusion-webui/extensions/stable-diffusion-webui-huggingface") 
     # os.system(f"git clone https://github.com/Cabel7/Webui/tree/bump-Pillow-blendmodes-dependency/modules /content/stable-diffusion-webui/modules") 
     # os.system(f"git clone https://github.com/XavierXiao/Dreambooth-Stable-Diffusion /content/stable-diffusion-webui/extensions/Dreambooth-Stable-Diffusion") 
-    # os.system(f"rm -rf /content/stable-diffusion-webui/modules")
-    #os.system(f"wget https://github.com/AUTOMATIC1111/stable-diffusion-webui/tree/master/modules /content/stable-diffusion-webui/modules")
+    os.system(f"rm -rf /content/stable-diffusion-webui/modules")
+    # os.system(f"wget https://github.com/AUTOMATIC1111/stable-diffusion-webui/tree/master/modules /content/stable-diffusion-webui/modules")
     os.system(f"git clone https://github.com/Cabel7/modules /content/stable-diffusion-webui/modules") 
 
 
@@ -59,15 +64,17 @@ def run():
     # os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/ckpt/juggernaut-xl/resolve/main/juggernautXL_version2.safetensors -d /content/stable-diffusion-webui/models/Stable-diffusion -o juggernautXL_version2.safetensors")
     # os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/ckpt/sd_xl_refiner_1.0/resolve/main/sd_xl_refiner_1.0_0.9vae.safetensors -d /content/stable-diffusion-webui/models/Stable-diffusion -o sd_xl_refiner_1.0_0.9vae.safetensors")
     # os.system(f"sed -i -e 's/\["sd_model_checkpoint"\]/\["sd_model_checkpoint","sd_vae","CLIP_stop_at_last_layers"\]/g' /content/stable-diffusion-webui/modules/shared_options.py") 
-    os.environ['HF_HOME'] = '/content/stable-diffusion-webui/cache/huggingface'
-    # os.system(f"python launch.py --cors-allow-origins=* --xformers --theme dark --gradio-debug --share --enable-insecure-extension-access")
-    sys.path.append('/content/stable-diffusion-webui')
-    sys.argv = shlex.split("--cors-allow-origins=* --xformers --theme dark --gradio-debug --share --no-half-vae --enable-insecure-extension-access")
-    from modules import launch_utils
-    launch_utils.startup_timer.record("initial startup")
-    launch_utils.prepare_environment()
-    launch_utils.start()
-
-@app.local_entrypoint()
-def main():
-    run.remote()
+    subprocess.Popen(
+        [
+            "python",
+            "launch.py",
+            "--listen",
+            "0.0.0.0",
+            "--port",
+            str(PORT),
+            "--always-high-vram",
+            "--xformers",
+            "--theme dark",
+            "--gradio-debug",
+        ]
+    )
