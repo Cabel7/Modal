495b6fc69a0dee8bd6e5ca2b904b0bf210752dca
Askerin <134921117+Cabel7@users.noreply.github.com>
2024-05-05 14:36:22 +0530

    Update run5.py

--- run5.py
+++ run5.py
@@ -30,17 +30,11 @@ app = modal.App("stable-diffusion-webui")
     timeout=60000,
 )
 
-
-#app = modal.App("stable-diffusion-webui", image=image)
-
-
-#@app.function(gpu="T4", timeout=60000)
-
 async def run():
-    # os.system(f"git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git /content/stable-diffusion-webui")
+    os.system(f"git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git /content/stable-diffusion-webui")
     # os.system(f"git clone https://github.com/Cabel7/Webui /content/stable-diffusion-webui")
     
-    os.system(f"git clone -b v2.6 https://github.com/camenduru/stable-diffusion-webui /content/stable-diffusion-webui")
+    # os.system(f"git clone -b v2.6 https://github.com/camenduru/stable-diffusion-webui /content/stable-diffusion-webui")
     os.system(f"git clone https://github.com/camenduru/sd-civitai-browser /content/stable-diffusion-webui/extensions/sd-civitai-browser")
     os.system(f"git clone https://github.com/DominikDoom/a1111-sd-webui-tagcomplete.git /content/stable-diffusion-webui/extensions/tag-autocomplete") 
     os.system(f"git clone https://github.com/camenduru/stable-diffusion-webui-huggingface /content/stable-diffusion-webui/extensions/stable-diffusion-webui-huggingface") 
