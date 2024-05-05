34c88fb56c62bee892dad58ab8db83cc42e8d5fb
Askerin <134921117+Cabel7@users.noreply.github.com>
2024-05-05 13:36:39 +0530

    Update run5.py

--- run5.py
+++ run5.py
@@ -26,14 +26,15 @@ app = modal.App("stable-diffusion-webui")
         .run_commands("pip install -q torch==2.0.1+cu118 torchvision==0.15.2+cu118 torchaudio==2.0.2+cu118 torchtext==0.15.2 torchdata==0.6.1 --extra-index-url https://download.pytorch.org/whl/cu118")
         .run_commands("pip install -q xformers==0.0.20 triton==2.0.0 packaging==23.1")
         .run_commands("git clone -b v2.6 https://github.com/camenduru/stable-diffusion-webui /content/stable-diffusion-webui")
-    )  
+    ),
+    gpu="T4",
 )
 
 
 #app = modal.App("stable-diffusion-webui", image=image)
 
 
-@app.function(gpu="T4", timeout=60000)
+#@app.function(gpu="T4", timeout=60000)
 
 async def run():
     # os.system(f"git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git /content/stable-diffusion-webui")
