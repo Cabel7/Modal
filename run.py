2af25e9d3ab86fe3ffcdb7c8ebe51b6bf132b906
Askerin <134921117+Cabel7@users.noreply.github.com>
2024-05-25 19:23:14 +0530

    Update run.py

--- run.py
+++ run.py
@@ -26,9 +26,7 @@ app = modal.App("stable-diffusion-webui")
         )
         .run_commands("pip install -q torch==2.0.1+cu118 torchvision==0.15.2+cu118 torchaudio==2.0.2+cu118 torchtext==0.15.2 torchdata==0.6.1 --extra-index-url https://download.pytorch.org/whl/cu118")
         .run_commands("pip install -q xformers==0.0.20 triton==2.0.0 packaging==23.1")
-        .run_commands("pip install bitsandbytes")
-        .run_commands("export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-12.2/targets/x86_64-linux/lib/")
-        
+      
     ),
     gpu="T4",
     timeout=60000,
@@ -36,6 +34,7 @@ app = modal.App("stable-diffusion-webui")
 
 async def run():
     os.system(f"git clone https://github.com/Cabel7/Stable-Diffusion /content/stable-diffusion-webui")
+    os.system(f"export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-12.2/targets/x86_64-linux/lib")
     # os.system(f"git clone https://github.com/Cabel7/Webui /content/stable-diffusion-webui")
     # os.system(f"git clone -b v2.6 https://github.com/camenduru/stable-diffusion-webui /content/stable-diffusion-webui")
     #os.system(f"git clone https://github.com/camenduru/sd-civitai-browser /content/stable-diffusion-webui/extensions/sd-civitai-browser")
