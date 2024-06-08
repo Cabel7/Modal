c81f3be8b6d8f2cb579ced0b5569f73282885d2c
Askerin <134921117+Cabel7@users.noreply.github.com>
2024-06-09 03:22:16 +0530

    Update run2.py

--- run2.py
+++ run2.py
@@ -23,15 +23,15 @@ async def run():
     os.system(f"git clone https://github.com/Cabel7/stable-diffusion-webui /content/stable-diffusion-webui")
     os.system(f"git clone https://github.com/d8ahazard/sd_dreambooth_extension.git /content/stable-diffusion-webui/extensions/Dreambooth-Stable-Diffusion") 
     
-    os.chdir(f"/home/studio-lab-user/content/stable-diffusion-webui")
+    os.chdir(f"/content/stable-diffusion-webui")
     # os.system(f"rm -rf /home/studio-lab-user/content/stable-diffusion-webui/repositories")
     os.system(f"git reset --hard")
     os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/Jagerblue/Dream/resolve/main/Quix_5843.safetensors -d /content/stable-diffusion-webui/models/Stable-diffusion -o Quix_5843.safetensors")
     # os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/ckpt/xxmix9realistic/resolve/main/xxmix9realistic_v30.safetensors -d /home/studio-lab-user/content/stable-diffusion-webui/models/Stable-diffusion -o xxmix9realistic_v30.safetensors")
     # os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/ckpt/sd-vae-ft-mse-original/resolve/main/vae-ft-mse-840000-ema-pruned.ckpt -d /home/studio-lab-user/content/stable-diffusion-webui/models/Stable-diffusion -o xxmix9realistic_v30.vae.pt")
-    os.environ['HF_HOME'] = '/home/studio-lab-user/content/stable-diffusion-webui/cache/huggingface'
+    os.environ['HF_HOME'] = '/content/stable-diffusion-webui/cache/huggingface'
     # os.system(f"python launch.py --cors-allow-origins=* --xformers --theme dark --gradio-debug --share")
-    sys.path.append('/home/studio-lab-user/content/stable-diffusion-webui')
+    sys.path.append('/content/stable-diffusion-webui')
     sys.argv = shlex.split("--cors-allow-origins=* --xformers --theme dark --gradio-debug --share --enable-insecure-extension-access --disable-safe-unpickle")
     from modules import launch_utils
     launch_utils.startup_timer.record("initial startup")
